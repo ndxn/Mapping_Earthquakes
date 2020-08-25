7984590b85fac62ae3aa3fdba986ce4adb401131
Neal Dixon <neal@Neals-MacBook-Pro-16.local>
2020-08-25 14:53:56 -0700

    Updated logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // Alternate method to create the map object with a center and zoom level,
 // with {}, useful when we need to add multiple tile layers, or a background image of our map(s)
@@ -14,7 +14,24 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //   });
 
 // Create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+
+//  Add a marker to the map for Los Angeles, California
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add circle  to map
+L.circle([34.0522, -118.2437], {
+    radius: 100,
+    color: "black",
+    fillColor: '#ffffa1'
+ }).addTo(map);
+
+// L.circleMarker([34.0522, -118.2437], {
+//     radius: 30,
+//     color: "black",
+//     fillColor: '#ffffa1'
+// }).addTo(map);
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     // id sets the map style, including options such as mapbox.streets, .light,.dark, .satellite, .streets-satellite, .wheatpaste, .streets-basic, .comic, .outdoors, .run-bike-hike, .pirates, .emerald, .high-contrast
