3bb5ce2f11a8c9ec2939c2629320c555b018be93
Neal Dixon <neal@Neals-MacBook-Pro-16.local>
2020-08-25 16:27:58 -0700

    Modified logic.js. and added cities.js

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // Alternate method to create the map object with a center and zoom level,
 // with {}, useful when we need to add multiple tile layers, or a background image of our map(s)
@@ -15,23 +15,61 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // Create the tile layer that will be the background of our map.
 
-//  Add a marker to the map for Los Angeles, California
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Adding cities as an array
+// let cities = [{
+//     location: [40.7128, -74.0059],
+//     city: "New York City",
+//     state: "NY",
+//     population: 8398748
+//   },
+//   {
+//     location: [41.8781, -87.6298],
+//     city: "Chicago",
+//     state: "IL",
+//     population: 2705994
+//   },
+//   {
+//     location: [29.7604, -95.3698],
+//     city: "Houston",
+//     state: "TX",
+//     population: 2325502
+//   },
+//   {
+//     location: [34.0522, -118.2437],
+//     city: "Los Angeles",
+//     state: "CA",
+//     population: 3990456
+//   },
+//   {
+//     location: [33.4484, -112.0740],
+//     city: "Phoenix",
+//     state: "AZ",
+//     population: 1660272
+//   }
+//   ];
 
-// Add circle  to map
-L.circle([34.0522, -118.2437], {
-    radius: 100,
-    color: "black",
-    fillColor: '#ffffa1'
- }).addTo(map);
+// // Loop through the cities array and create one marker for each city.
+// cities.forEach(function(city) {
+// 	console.log(city)
+// 	L.marker(city.location).addTo(map);
+// });
 
-// L.circleMarker([34.0522, -118.2437], {
-//     radius: 30,
-//     color: "black",
-//     fillColor: '#ffffa1'
-// }).addTo(map);
+// Get data from cities.js
+let cityData = cities;
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+	console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     // id sets the map style, including options such as mapbox.streets, .light,.dark, .satellite, .streets-satellite, .wheatpaste, .streets-basic, .comic, .outdoors, .run-bike-hike, .pirates, .emerald, .high-contrast
